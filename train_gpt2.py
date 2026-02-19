28cfb5acfee3868c12ca417648d0a10123d0703e
awaisnazir08 <owaisnazir2228@gmail.com>
2026-02-19 23:31:34 +0500

    Add Block class wrapping the multiheaded/casual attention

--- train_gpt2.py
+++ train_gpt2.py
@@ -39,6 +39,19 @@ class CausalSelfAttention(nn.Module):
         y = self.c_proj(y)
         return y
 
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
