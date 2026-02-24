038fff1304a8b3c0e1197584090a7b6955e06991
awaisnazir08 <awais.nazir@imagine.art>
2026-02-24 14:53:06 +0500

    Add simple dataloader

--- train_gpt2.py
+++ train_gpt2.py
@@ -163,6 +163,36 @@ class GPT(nn.Module):
 
 # ----------------------------------------------------------------
 
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+    
+        # at init load tokens from the disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"Loaded {len(self.tokens)} tokens from the dataset")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+        
+        # state
+        self.current_position = 0
+        
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T + 1] # (B * T)
+        x = buf[:-1].view(B, T) # (B, T)
+        y = buf[1:].view(B, T) # (B, T)
+        # advance the current position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + B*T + 1 >= len(self.tokens):
+            self.current_position = 0
+        return x, y
 
 # ----------------------------------------------------------------
 
